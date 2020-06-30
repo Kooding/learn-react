894c0feab28175823acea8fad5488354a19cc050
Kooding <giyun529@hanmail.net>
2020-06-30 19:13:19 +0900

    count 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useMemo } from "react";
 import AddableUserList from "./AddableUserList";
 import AddForm from "./AddForm";
+
+function countActiveUser(users) {
+	console.log("활성화수 세는중 ....");
+	return users.filter((user) => user.active).length;
+}
 
 function App() {
 	const [users, setUsers] = useState([
@@ -8,33 +13,33 @@ function App() {
 			id: 1,
 			username: "admin",
 			email: "admin@help.com",
-			active: true
+			active: true,
 		},
 		{
 			id: 2,
 			username: "guest",
 			email: "guest@guest.co.kr",
-			active: false
+			active: false,
 		},
 		{
 			id: 3,
 			username: "user",
 			email: "user@userhome.co.kr",
-			active: false
-		}
+			active: false,
+		},
 	]);
 	const [inputs, setInputs] = useState({
 		username: "",
-		email: ""
+		email: "",
 	});
 	const { username, email } = inputs;
 	// useing to users array in property id
 	const nextId = useRef(4);
-	const onChange = e => {
+	const onChange = (e) => {
 		const { name, value } = e.target;
 		setInputs({
 			...inputs,
-			[name]: value
+			[name]: value,
 		});
 	};
 
@@ -43,29 +48,30 @@ function App() {
 			{
 				id: nextId.current,
 				username,
-				email
-			}
+				email,
+			},
 		];
 		setUsers(users.concat(newUser));
 		setInputs({
 			username: "",
-			email: ""
+			email: "",
 		});
 		nextId.current += 1;
 	};
 
-	const onDelete = id => {
+	const onDelete = (id) => {
 		console.log(id);
-		setUsers(users.filter(user => user.id !== id));
+		setUsers(users.filter((user) => user.id !== id));
 	};
 
-	const onToggle = id => {
+	const onToggle = (id) => {
 		setUsers(
-			users.map(user =>
+			users.map((user) =>
 				user.id === id ? { ...user, active: !user.active } : user
 			)
 		);
 	};
+	const count = useMemo(() => countActiveUser(users), [users]);
 	return (
 		<>
 			<AddForm
@@ -74,11 +80,8 @@ function App() {
 				onChange={onChange}
 				onCreateUser={onCreateUser}
 			/>
-			<AddableUserList
-				users={users}
-				onDelete={onDelete}
-				onToggle={onToggle}
-			/>
+			<AddableUserList users={users} onDelete={onDelete} onToggle={onToggle} />
+			<div>현재 활성화된 카운트 수 : {count}</div>
 		</>
 	);
 }
